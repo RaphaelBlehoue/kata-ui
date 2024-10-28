f8d721cc45f0d49674b20032615f8fc3069594fc
SAMPAN Blehoue Hermann Raphael <raphaelsampan@gmail.com>
2024-10-28 23:52:46 +0100

    build: init local dev environment

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+    tsconfigPaths()
+  ],
+  server: {
+    port: 13370,
+  }
+});
